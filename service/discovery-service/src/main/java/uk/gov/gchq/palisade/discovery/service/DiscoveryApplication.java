a2c9284ee2ab467e896982d91b5a257920df3d83
dev-958 <wolfie.acb@gmail.com>
2019-06-26 15:55:29 +0100

    Added Spring Boot parent to master pom, docker build discovery service and add banner.

--- service/discovery-service/src/main/java/uk/gov/gchq/palisade/discovery/service/DiscoveryApplication.java
+++ service/discovery-service/src/main/java/uk/gov/gchq/palisade/discovery/service/DiscoveryApplication.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2018 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.gchq.palisade.discovery.service;
 
 import org.springframework.boot.WebApplicationType;
@@ -9,8 +25,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class DiscoveryApplication {
 
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(DiscoveryApplication.class).web(WebApplicationType.SERVLET).run(args);
+    public static void main(final String[] args) {
+        new SpringApplicationBuilder(DiscoveryApplication.class).web(WebApplicationType.SERVLET)
+                .run(args);
     }
 
 }
